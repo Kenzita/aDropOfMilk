22b76db6b741e29e3d47188cfbe93969b2cfdf5b
kenzita <kenza.skalli@yahoo.fr>
2023-11-22 16:20:35 +0100

    update App

--- Front/src/App.js
+++ Front/src/App.js
@@ -5,15 +5,6 @@ import Footer from "./components/Footer/Footer"
 
 function App() {
 
-    // const [seeComponent, setSeeComponent] = useState(1);
-    // const [user, setUser] = useState(null);
-
-    // console.log(user);
-
-    // const getUser = (userLogged) => {
-    //     setUser(userLogged);
-    // };
-
     return (
         <div className={`d-flex flex-column  ${styles.appContainer}`}>
             
